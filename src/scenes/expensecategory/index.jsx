38b1ba17a4b1688afcc1cd853e3dac28fc0cad47
Mohd Afan <mohdafan@Mohd-Afan.local>
2023-05-06 15:25:05 +0530

    create expense Category

--- src/scenes/expensecategory/index.jsx
+++ src/scenes/expensecategory/index.jsx
@@ -4,9 +4,8 @@ import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 import Layout from "../../Laybout";
-import { collection, doc, getFirestore, setDoc } from 'firebase/firestore';
-import { addDoc } from "firebase/firestore";
-import { DB } from "../../firebase-config";
+import { collection, doc, getFirestore, setDoc, addDoc } from 'firebase/firestore';
+import { db } from "../../firebase-config";
 import { useCollection } from 'react-firebase-hooks/firestore';
 import {app} from '../../firebase-config'
 import { useNavigate } from "react-router-dom";
@@ -14,30 +13,27 @@ import { useNavigate } from "react-router-dom";
 const Form = () => {
   const navigate = useNavigate()
 
-  const [expenseCategory] = useCollection(
-    collection(getFirestore(app), "expenseCategory"),
-    {
-      snapshotListenOptions: { includeMetadataChanges: true },
-    }
-  );
+  // const [expenseCategory] = useCollection(
+  //   collection(getFirestore(app), "expenseCategory"),
+  //   {
+  //     snapshotListenOptions: { includeMetadataChanges: true },
+  //   }
+  // );
 
   const isNonMobile = useMediaQuery("(min-width:600px)");
 
   const handleFormSubmit = async (values) => {
-    try {
-      await addDoc(collection(DB, "expenseCategory"), {
-        expenceCategory: values.expenseCategory,
+    try{
+      await addDoc(collection(db, "expenseCategory"), {
+        expenseCategory: values.expenseCategory,
         budgets: values.budgets,
-        description: values.description,
+        description: values.description
       });
-      // enqueueSnackbar('Campaign Created  successful!');
-      navigate('/');
-      console.log('Created')
+      navigate('/')
+    }catch(err){
+      console.log(err);
     }
-    catch (error) {
-      console.error(error);
-    }
-    console.log(values);
+    
   };
 
   return (
@@ -83,7 +79,7 @@ const Form = () => {
               <TextField
                 fullWidth
                 variant="filled"
-                type="text"
+                type="number"
                 label="Budgets $"
                 onBlur={handleBlur}
                 onChange={handleChange}
@@ -122,18 +118,15 @@ const Form = () => {
   );
 };
 
-const phoneRegExp =
-  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
-
 const checkoutSchema = yup.object().shape({
   expenseCategory: yup.string().required("required"),
-  budgets: yup.string().required("required"),
+  budgets: yup.number().required("required"),
   description: yup.string(),
   
 });
 const initialValues = {
   expenseCategory: "",
-  budgets: "",
+  budgets: 0,
   description: "",
   
 };
